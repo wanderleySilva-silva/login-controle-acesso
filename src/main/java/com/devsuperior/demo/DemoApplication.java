662e696b3a6fe9f1e707dabbd94a5735e2260f52
wanderleySilva-silva <jwps.17@hotmail.com>
2025-07-15 13:32:02 -0300

    feat: Adicionados roles e spring security

---
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.devsuperior.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("ENCODER: " + passwordEncoder.encode("123456"));
+	}
+}
